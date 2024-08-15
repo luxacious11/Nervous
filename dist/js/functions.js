332ea0bbaa83d7b68e6c9504863e5eb153b0162a
Dawn Eggleton <dawn.eggleton@Dawns-MacBook-Air.local>
2024-08-15 16:10:22 -0300

    add public and dist files

---
+++ dist/js/functions.js
@@ -0,0 +1,445 @@
+function getAllTextNodes(element) {
+    if(element) {
+        return Array.from(element.childNodes).filter(node => node.nodeType === 3 && node.textContent.trim().length > 1);
+    }
+}
+function inputWrap(el, next = null, type = 'checkbox') {
+    if(next) {
+        $(el).nextUntil(next).andSelf().wrapAll(`<label class="input-wrap ${type}"></label>`);
+    } else {
+        $(el).next().andSelf().wrapAll(`<label class="input-wrap ${type}"></label>`);
+    }
+}
+function fancyBoxes(type = 'checkbox') {
+    document.querySelectorAll('.input-wrap.checkbox').forEach(label => {
+        label.querySelector('input').insertAdjacentHTML('afterend', `<div class="fancy-input checkbox">x</div>`);
+    });
+    document.querySelectorAll('.input-wrap.radio').forEach(label => {
+        label.querySelector('input').insertAdjacentHTML('afterend', `<div class="fancy-input radio">x</div>`);
+    });
+}
+
+/****** Settings ******/
+function setTheme() {
+    if(localStorage.getItem('theme') !== null) {
+        switch(localStorage.getItem('theme')) {
+            case 'light':
+                document.querySelector('body').classList.remove('dark');
+                document.querySelector('body').classList.add('light');
+                break;
+            case 'dark':
+            default:
+                document.querySelector('body').classList.add('dark');
+                document.querySelector('body').classList.remove('light');
+                break;
+        }
+    } else {
+        document.querySelector('body').classList.add('dark');
+        document.querySelector('body').classList.remove('light');
+        localStorage.setItem('theme', 'dark');
+    }
+}
+function setSize() {
+    if(localStorage.getItem('size') !== null) {
+        switch(localStorage.getItem('size')) {
+            case 'xl':
+                document.querySelector('body').classList.remove('smFont');
+                document.querySelector('body').classList.remove('lgFont');
+                document.querySelector('body').classList.add('xlFont');
+                break;
+            case 'large':
+                document.querySelector('body').classList.remove('smFont');
+                document.querySelector('body').classList.add('lgFont');
+                document.querySelector('body').classList.remove('xlFont');
+                break;
+            case 'small':
+            default:
+                document.querySelector('body').classList.remove('lgFont');
+                document.querySelector('body').classList.add('smFont');
+                document.querySelector('body').classList.remove('xlFont');
+                break;
+        }
+    } else {
+        document.querySelector('body').classList.remove('xlFont');
+        document.querySelector('body').classList.remove('lgFont');
+        document.querySelector('body').classList.add('smFont');
+        localStorage.setItem('size', 'small');
+    }
+}
+
+/****** Toggles ******/
+function toggleTheme() {
+    if(localStorage.getItem('theme') === 'dark') {
+        localStorage.setItem('theme', 'light');
+        setTheme();
+    } else {
+        localStorage.setItem('theme', 'dark');
+        setTheme();
+    }
+}
+function toggleSize() {
+    if(localStorage.getItem('size') === 'small') {
+        localStorage.setItem('size', 'large');
+        setSize();
+    } else if(localStorage.getItem('size') === 'large') {
+        localStorage.setItem('size', 'xl');
+        setSize();
+    } else {
+        localStorage.setItem('size', 'small');
+        setSize();
+    }
+}
+function toggleMenu(e) {
+    let close = false;
+    if(e.classList.contains('is-open')) {
+        close = true;
+    }
+    if(e.dataset.menu) {
+        document.querySelectorAll('.nav--popout').forEach(menu => menu.classList.remove('is-open'));
+        document.querySelectorAll('.button--menu').forEach(menu => menu.classList.remove('is-open'));
+
+        if(!close) {
+            e.classList.add('is-open');
+            document.querySelector(`.nav--popout[data-menu="${e.dataset.menu}"]`).classList.add('is-open');
+            document.querySelector('.invisibleEl').classList.add('menu-open');
+        } else {
+            document.querySelector('.invisibleEl').classList.remove('menu-open');
+        }
+    } else {
+        document.querySelectorAll('.nav--popout').forEach(menu => menu.classList.remove('is-open'));
+        document.querySelectorAll('.button--menu').forEach(menu => menu.classList.remove('is-open'));
+
+        if(!close) {
+            e.classList.add('is-open');
+            e.closest('.nav--inline').querySelector('.nav--popout').classList.add('is-open');
+            document.querySelector('.invisibleEl').classList.add('menu-open');
+        } else {
+            document.querySelector('.invisibleEl').classList.remove('menu-open');
+        }
+    }
+}
+
+/****** Global Initialization ******/
+function initClipboard() {
+    let clipboard = new Clipboard('.clipboard');
+    clipboard.on('success', function(e) {
+        console.log('clipboard success: ' + e);
+    });
+    clipboard.on('error', function(e) {
+        console.log('clipboard error: ' + e);
+    });
+    let clipcode = new Clipboard('.codeclick', {
+        target: function(trigger) {
+        return trigger.nextElementSibling;
+        }
+    });
+}
+function initCodebox() {
+    $("table[id='CODE-WRAP']").each(function() {
+        var cc = $(this).find("td[id='CODE']").html();
+
+        $(this).html(
+            "<div class='codeblock code--wrapper'><div class='c-title codeclick'>Click to Copy</div><div class='codecon'><pre><code class='scroll'>"
+            + cc +
+            "</code></pre></div></div>"
+        );
+    });
+}
+function initCopyLink() {
+    let clippedURL = new Clipboard('.post--permalink');
+    document.querySelectorAll('.post--permalink').forEach(link => {
+        link.addEventListener('click', e => {
+            e.currentTarget.querySelector('.note').style.opacity = 1;
+            setTimeout(() => {
+                document.querySelectorAll('.note').forEach(note => note.style.opacity = 0);
+            }, 3000);
+        });
+    });
+}
+function initQuickLogin() {
+    if($('#quick-login').length) {
+        $('#quick-login').appendTo('#quick-login-clip');
+        document.querySelector('#quick-login-clip input[name="UserName"]').setAttribute('placeholder', 'Username');
+        document.querySelector('#quick-login-clip input[name="PassWord"]').setAttribute('placeholder', 'Password');
+    } else {
+        var main_url = location.href.split('?')[0];
+        $.get(main_url, function (data) {
+            $('#quick-login', data).appendTo('#quick-login-clip');
+            document.querySelector('#quick-login-clip input[name="UserName"]').setAttribute('placeholder', 'Username');
+            document.querySelector('#quick-login-clip input[name="PassWord"]').setAttribute('placeholder', 'Password');
+        });
+    }
+}
+function initSwitcher() {
+	let characters = switcher.querySelectorAll('option');
+	let newSwitch = `<div class="switch">`;
+	characters.forEach((character, i) => {
+		if(i !== 0) {
+			let characterName = character.innerText.trim();
+			let characterId = character.value;
+			newSwitch += `<label class="switch--block">
+				<input type="checkbox" value="${characterId}" onchange="this.form.submit()" name="sub_id" />
+				${createAvatars(`switch--image`, characterId)}
+				<b>${capitalize(characterName)}</b>
+			</label>`;
+		}
+	});
+	newSwitch += `</div>`;
+	switcher.insertAdjacentHTML('afterend', newSwitch);
+	switcher.remove();
+}
+function formatMarkdown(str, identifier, opening, closing) {
+    str = str
+      .split(identifier)
+      .map((value, index) => {
+        if(str.split(identifier).length - 1 !== index) {
+            if ((value.includes('href=') ||
+            value.includes('target=') ||
+            value.includes('src=') ||
+            value.includes('class=') ||
+            value.includes('')) &&
+            str.split(identifier).length > 1) {
+                return `${value}${identifier}`;
+            } else if(index % 2 == 0) {
+                return value;
+            } else {
+                return `${opening}${value}${closing}`;
+            }
+        } else {
+            return value;
+        }
+      }).join('');
+    return str;
+}
+function initMarkdown() {
+    let quickLists = document.querySelectorAll('tl');
+    if(quickLists.length > 0) {
+        quickLists.forEach(list => {
+            list.innerHTML = formatQuickList(list);
+        });
+    }
+
+    if(document.querySelectorAll(markdownSafe).length > 0) {
+        document.querySelectorAll(markdownSafe).forEach(post => {
+            let str = post.innerHTML;
+            str = formatMarkdown(str, `**`, `<b>"`, `"</b>`);
+            str = formatMarkdown(str, `_`, `<i>`, `</i>`);
+            str = formatMarkdown(str, `||`, `<tag-spoiler>`, `</tag-spoiler>`);
+            post.innerHTML = str;
+        });
+    }
+
+    let spoilers = document.querySelectorAll('tag-spoiler');
+    if(spoilers.length > 0) {
+        spoilers.forEach(spoiler => {
+            spoiler.addEventListener('click', e => {e.currentTarget.classList.add('is-visible')});
+        });
+    }
+}
+
+/****** Utilities ******/
+function fixMc(str) {
+    return (""+str).replace(/Mc(.)/g, function(m, m1) {
+        return 'Mc' + m1.toUpperCase();
+    });
+}
+function fixMac(str) {
+    return (""+str).replace(/Mac(.)/g, function(m, m1) {
+        return 'Mac' + m1.toUpperCase();
+    });
+}
+function capitalize(str, separators = [` `, `'`, `-`]) {
+    str = str.replaceAll(`\&\#39\;`, `'`);
+    separators = separators || [ ' ' ];
+    var regex = new RegExp('(^|[' + separators.join('') + '])(\\w)', 'g');
+    let first = str.split(' ')[0].replace(regex, function(x) { return x.toUpperCase(); });
+    let last = fixMac(fixMc(str.split(' ').slice(1).join(' ').replace(regex, function(x) { return x.toUpperCase(); })));
+    return `${first} ${last}`;
+}
+function capitalizeMultiple(selector) {
+    document.querySelectorAll(selector).forEach(character => {
+        character.innerText = capitalize(character.innerText);
+    });
+}
+function setMonth(month) {
+    switch(month) {
+        case 'January':
+            month = 1;
+            break;
+        case 'February':
+            month = 2;
+            break;
+        case 'March':
+            month = 3;
+            break;
+        case 'April':
+            month = 4;
+            break;
+        case 'May':
+            month = 5;
+            break;
+        case 'June':
+            month = 6;
+            break;
+        case 'July':
+            month = 7;
+            break;
+        case 'August':
+            month = 8;
+            break;
+        case 'September':
+            month = 9;
+            break;
+        case 'October':
+            month = 10;
+            break;
+        case 'November':
+            month = 11;
+            break;
+        case 'December':
+            month = 12;
+            break;
+        default:
+            month = -1;
+            break;
+    }
+
+    return month;
+}
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+function rgbToHex(r, g, b) {
+    return componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+function cleanText(text) {
+	return text.replaceAll(' ', '').replaceAll('&amp;', '').replaceAll('&', '').replaceAll(`'`, '').replaceAll(`"`, '').replaceAll(`.`, '').replaceAll(`(`, '').replaceAll(`)`, '').replaceAll(`,`, '').replaceAll(`’`, '').replaceAll(`é`, `e`).replaceAll(`è`, `e`).replaceAll(`ê`, `e`).replaceAll(`ë`, `e`).replaceAll(`ě`, `e`).replaceAll(`ẽ`, `e`).replaceAll(`ē`, `e`).replaceAll(`ė`, `e`).replaceAll(`ę`, `e`).replaceAll(`à`, `a`).replaceAll(`á`, `a`).replaceAll(`â`, `a`).replaceAll(`ä`, `a`).replaceAll(`ǎ`, `a`).replaceAll(`æ`, `ae`).replaceAll(`ã`, `a`).replaceAll(`å`, `a`).replaceAll(`ā`, `a`).replaceAll(`í`, `i`).replaceAll(`ì`, `i`).replaceAll(`ı`, `i`).replaceAll(`î`, `i`).replaceAll(`ï`, `i`).replaceAll(`ǐ`, `i`).replaceAll(`ĭ`, `i`).replaceAll(`ī`, `i`).replaceAll(`ĩ`, `i`).replaceAll(`į`, `i`).replaceAll(`ḯ`, `i`).replaceAll(`ỉ`, `i`).replaceAll(`ó`, `o`).replaceAll(`ò`, `o`).replaceAll(`ȯ`, `o`).replaceAll(`ô`, `o`).replaceAll(`ö`, `o`).replaceAll(`ǒ`, `o`).replaceAll(`ŏ`, `o`).replaceAll(`ō`, `o`).replaceAll(`õ`, `o`).replaceAll(`ǫ`, `o`).replaceAll(`ő`, `o`).replaceAll(`ố`, `o`).replaceAll(`ồ`, `o`).replaceAll(`ø`, `o`).replaceAll(`ṓ`, `o`).replaceAll(`ṑ`, `o`).replaceAll(`ȱ`, `o`).replaceAll(`ṍ`, `o`).replaceAll(`ú`, `u`).replaceAll(`ù`, `u`).replaceAll(`û`, `u`).replaceAll(`ü`, `u`).replaceAll(`ǔ`, `u`).replaceAll(`ŭ`, `u`).replaceAll(`ū`, `u`).replaceAll(`ũ`, `u`).replaceAll(`ů`, `u`).replaceAll(`ų`, `u`).replaceAll(`ű`, `u`).replaceAll(`ʉ`, `u`).replaceAll(`ǘ`, `u`).replaceAll(`ǜ`, `u`).replaceAll(`ǚ`, `u`).replaceAll(`ṹ`, `u`).replaceAll(`ǖ`, `u`).replaceAll(`ṻ`, `u`).replaceAll(`ủ`, `u`).replaceAll(`ȕ`, `u`).replaceAll(`ȗ`, `u`).replaceAll(`ư`, `u`);
+}
+function filterValue(e) {
+    let searchValue = e.value.toLowerCase().trim();
+    let names = document.querySelectorAll(`[data-key="${e.dataset.filter}"] .claim ${e.dataset.objects}`);
+    let headers = document.querySelectorAll(`[data-key="${e.dataset.filter}"] ${e.dataset.headers}`);
+    let wraps = document.querySelectorAll(`[data-key="${e.dataset.filter}"] .claim-wrap`);
+    if(searchValue !== '') {
+        e.parentNode.classList.add('pb');
+        e.closest('.scroll').querySelectorAll('.accordion--trigger, .accordion--content').forEach(item => item.classList.add('is-active'));
+        names.forEach(name => {
+            let nameValue = name.innerText.toLowerCase().trim();
+            if (nameValue.indexOf(searchValue) > -1) {
+                name.closest('.claim').classList.remove('hidden');
+            } else {
+                name.closest('.claim').classList.add('hidden');
+            }
+            let childrenArray = Array.from(name.closest('.claim-wrap').querySelectorAll('.claim')).filter(item => !item.classList.contains('hidden'));
+            if(childrenArray.length === 0) {
+                name.closest('.claim-wrap').previousElementSibling.classList.add('hidden');
+                if (e.dataset.hideWrap === 'true') {
+                            name.closest('.claim-wrap').classList.add('hidden');
+                }
+            } else {
+                name.closest('.claim-wrap').previousElementSibling.classList.remove('hidden');
+                if (e.dataset.hideWrap === 'true') {
+                            name.closest('.claim-wrap').classList.remove('hidden');
+                }
+            }
+        });
+    } else {
+        e.parentNode.classList.remove('pb');
+        headers.forEach(header => header.classList.remove('hidden'));
+        names.forEach(name => name.closest('.claim').classList.remove('hidden'));
+        wraps.forEach(wrap => wrap.classList.remove('hidden'));
+        e.closest('.scroll').querySelectorAll('.accordion--trigger, .accordion--content').forEach(item => item.classList.remove('is-active'));
+    }
+}
+function appendSearchQuery(param, value) {
+	const url = new URL(window.location.href);
+	url.searchParams.set(param, value);
+	window.history.replaceState(null, null, url);
+}
+function translationSwitch(e) {
+        let current = e.innerText;
+        let original = e.dataset.original;
+        let translation = e.dataset.result;
+        if(current === original) {
+            e.innerText = translation;
+        } else {
+            e.innerText = original;
+        }
+}
+function highlightCode() {
+    let clipcodeQuick = new Clipboard('.copyQuick', {
+        target: function(trigger) {
+            if(trigger.nextElementSibling.querySelector('textarea')) {
+                return trigger.nextElementSibling.querySelector('textarea');
+            } else {
+                return trigger.nextElementSibling.querySelector('code');
+            }
+        }
+    });
+}
+function getAllTextNodes(element) {
+    if(element) {
+        return Array.from(element.childNodes).filter(node => node.nodeType === 3 && node.textContent.trim().length > 1);
+    }
+}
+function getAllTextNodesArray(elements) {
+    let array = [];
+    if(elements) {
+        elements.forEach(element => {
+            let nodes = Array.from(element.childNodes).filter(node => node.nodeType === 3 && node.textContent.trim().length > 1);
+            if(nodes.length > 0) {
+                array = [...array, ...nodes];
+            }
+        });
+    }
+    return array;
+}
+function inputWrap(el, next = null, type = 'checkbox') {
+    if(next) {
+        $(el).nextUntil(next).andSelf().wrapAll(`<label class="input-wrap ${type}"></label>`);
+    } else {
+        $(el).next().andSelf().wrapAll(`<label class="input-wrap ${type}"></label>`);
+    }
+}
+function fancyBoxes() {
+    document.querySelectorAll('.input-wrap.checkbox').forEach(label => {
+        label.querySelector('input').insertAdjacentHTML('afterend', `<div class="fancy-input checkbox"><i class="fa-solid fa-check"></i></div>`);
+    });
+    document.querySelectorAll('.input-wrap.radio').forEach(label => {
+        label.querySelector('input').insertAdjacentHTML('afterend', `<div class="fancy-input radio"><i class="fa-solid fa-check"></i></div>`);
+    });
+}
+function read_alerts() {
+    let allMenus = document.querySelectorAll('.menu');
+    let allButtons = document.querySelectorAll('.button--menu');
+    allMenus.forEach(menu => menu.classList.remove('is-open'));
+    allButtons.forEach(button => button.classList.remove('is-open'));
+    document.querySelector('.invisibleEl').classList.remove('menu-open');
+    $.get( "index.php?recent_alerts=1&read=1", function( data ) {
+        $( "#recent_alerts_data" ).html( data );
+    });
+    document.querySelector(`button[data-menu=".nav--alerts"]`).dataset.new = 0;
+}
+function createAvatars(classes, id, attributes = ``) {
+    let html = `<div class="${classes}" style="background-image: `;
+    for(let i = 0; i < fileTypes.length; i++) {
+        html += `url(https://files.jcink.net/${uploads}/${siteName}/av-${id}.${fileTypes[i]}),`;
+    }
+    html += `url(${defaultSquare});" ${attributes}></div>`;
+    return html;
+}
+function formatQuickList(list) {
+    let html = ``;
+
+    if(list.innerHTML.split(`+ `).length > 0) {
+        html = `<ul>
+            ${list
+            .innerHTML.split(`+ `)
+            .filter(item => item !== '' && item !== '\n')
+            .map(item => `<li>${item}</li>`).join('')}
+        </ul>`;
+    }
+
+    return html;
+}
---
+++ dist/js/settings.js
@@ -0,0 +1,4 @@
+const uploads = `uploads2`;
+const siteName = `godlybehaviour`;
+const fileTypes = ['gif', 'jpg', 'jpeg', 'png'];
+const defaultSquare = `https://picsum.photos/200`;
